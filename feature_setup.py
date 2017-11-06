002b27ae43d539eaa65b47da4be145d8a7596ff4
Antonio Gonzalez <antoniogtv@gmail.com>
2017-11-05 17:35:50 -0800

    we beat them bois.. 98%

---
+++ feature_setup.py
@@ -0,0 +1,51 @@
+'''
+Feature setup for TitanicML
+'''
+
+import pandas as pd
+
+
+def setup_data(data): 
+	data["Title"] = 0
+
+	for i in data:
+		data['Title'] = data.Name.str.extract('([\w]+)\.', expand=True)
+	
+	# replaces unnecessary titles
+	data['Title'] = data["Title"].replace(['Mlle','Mme','Ms','Dr','Major','Lady','Countess','Jonkheer','Col','Rev','Capt','Sir','Don'],
+			['Miss','Miss','Miss','Mr','Mr','Mrs','Mrs','Other','Mr','Mr','Mr','Mr','Mr'])
+	data['Title'] = data["Title"].apply(lambda x: 'Mrs' if x == 'Dona' else x)
+	#print("Title feature successfully created.")
+
+	# used to observe the average age for each Title
+	#print(data.groupby('Title')['Age'].mean())
+	data.loc[(data.Age.isnull()) & (data.Title == 'Master'), 'Age'] = 5
+	data.loc[(data.Age.isnull()) & (data.Title == 'Miss'), 'Age'] = 22
+	data.loc[(data.Age.isnull()) & (data.Title == 'Mr'), 'Age'] = 33
+	data.loc[(data.Age.isnull()) & (data.Title == 'Mrs'), 'Age'] = 36
+	data.loc[(data.Age.isnull()) & (data.Title == 'Other'), 'Age'] = 38
+	data['Embarked'].fillna('S', inplace=True)
+
+	data['Age_Range'] = 0
+	data.loc[(data['Age'] <= 16, 'Age_Range')] = 0
+	data.loc[(16 < data['Age']) & (data['Age'] <= 32), 'Age_Range'] = 1
+	data.loc[(32 < data['Age']) & (data['Age'] <= 48), 'Age_Range'] = 2
+	data.loc[(48 < data['Age']) & (data['Age'] <= 64), 'Age_Range'] = 3
+	data.loc[(64 < data['Age']), 'Age_Range'] = 4
+
+	data['Fare_Split'] = pd.qcut(data['Fare'], 4)
+	#print(data.groupby(['Fare_Split'])['Survived'].mean())		
+	# creates Fare_Range
+	data['Fare_Range'] = 0
+	data.loc[data['Fare'] <= 7.91, 'Fare_Range'] = 0
+	data.loc[(7.91 < data['Fare']) & (data['Fare'] <= 14.45), 'Fare_Range'] = 1
+	data.loc[(14.45 < data['Fare']) & (data['Fare'] <= 31), 'Fare_Range'] = 2
+	data.loc[31 < data['Fare'], 'Fare_Range'] = 3
+
+	data['Sex'] = data['Sex'].replace(['male','female'], [0,1])
+	data['Embarked'] = data['Embarked'].replace(['S','C','Q'], [0,1,2])
+	data['Title'] = data['Title'].replace(['Mr','Mrs','Miss','Master','Other'], [0,1,2,3,4])
+
+	return data.drop(['Name','Age','Ticket','Fare','Cabin', 'Fare_Split','PassengerId'], axis=1)
+
+	print('All features setup successfully.')
--- main.py
+++ main.py
@@ -4,9 +4,79 @@ from Kaggle
 '''
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
 
-training_data = pd.read_csv("train.csv") 
+#classifiers
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.linear_model import LogisticRegression
 
-print(training_data.head())
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-training_data.isnull().sum()
+from feature_setup import setup_data
+
+
+def main():
+	#reads training data 
+	train_data = pd.read_csv("train.csv") 
+	
+	#reads and joins the two testing data files
+	test_data = pd.read_csv("test.csv")
+	test_labels = pd.read_csv("gender_submission.csv")
+	new_test_data = pd.concat([test_labels, test_data], axis=1)
+
+
+	# adjusts features to be used in classifier
+	train_data = setup_data(train_data)
+	new_test_data = setup_data(new_test_data)
+
+	#sets features and labels to the appropriate data
+	labels_train = train_data['Survived']
+	features_train = train_data.drop(['Survived'], axis=1)
+	labels_test = new_test_data['Survived']
+	features_test = new_test_data.drop(['Survived'], axis=1)
+
+	#print(features_test)
+
+	#CLASSIFIERS
+	svc = SVC(C=1.0, kernel='linear', gamma='auto')
+	naive_bayes = GaussianNB()
+	decision_tree = DecisionTreeClassifier()
+	forest = RandomForestClassifier()
+	knn = KNeighborsClassifier()
+	logic_reg = LogisticRegression()
+
+	#print(features_test)
+	#print(labels_train)
+
+	#fitting classifiers
+	accuracy_dict = {} 
+	clf_list = [('svc',svc), ('naive_bayes',naive_bayes), ('decsion_tree',decision_tree), ('random_forest', forest), ('knn',knn), ('logistic_regression',logic_reg)]
+	
+	for name,clf in clf_list:
+		print('Clf:', name, end=' ')
+		accuracy_dict[name] = test_classifier(clf, features_train, labels_train, features_test, labels_test)
+
+	#print(accuracy_dict)
+
+
+def test_classifier(clf, features_train, labels_train, features_test, labels_test):
+	clf.fit(features_train, labels_train)
+	predictions = clf.predict(features_test)
+	accuracy = accuracy_score(predictions, labels_test)
+	print("Accuracy:", accuracy)
+	return accuracy
+
+
+#####################################
+#Main function call
+
+if __name__ == '__main__':
+	main()
+
+#####################################
